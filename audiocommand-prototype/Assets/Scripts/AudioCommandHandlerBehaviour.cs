7f8792f91dc3d6abc006a8bebf05976e2fb67436
Luis Yucra <luis.yucra@gmail.com>
2018-01-26 20:51:20 -0500

    added test project for audio command

---
+++ audiocommand-prototype/Assets/Scripts/AudioCommandHandlerBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioCommandHandlerBehaviour : MonoBehaviour {
+
+	public AudioSource AudioSource;
+
+	// Use this for initialization
+	void Start () {
+		float[] samples = new float[AudioSource.clip.samples * AudioSource.clip.channels];
+		AudioSource.clip.GetData(samples, 0);
+
+		Debug.Log ("");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
