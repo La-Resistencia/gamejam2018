8ceb650cbe3530331bb000d27e66072cf47c5ce5
Luis Yucra <luis.yucra@gmail.com>
2018-01-27 09:28:38 -0500

    added signal visualizer

---
+++ audiocommand-prototype/Assets/Scripts/CommandDetectorBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Lomont;
+using UnityEngine;
+
+public class CommandDetectorBehaviour : MonoBehaviour {
+
+	// We assume that audio has a sampling 8 KHz
+	private const int SAMPLES = 4096; // 2 ^ 12
+	private const int DIVISOR = 15;
+	
+	private LomontFFT _fourierTransform;
+	private float[] _values;
+	
+	public void Start ()
+	{
+		var samples = GetComponents<AudioSource>();
+		
+		
+		
+	}
+	
+	public void Update () {
+		
+	}
+}
