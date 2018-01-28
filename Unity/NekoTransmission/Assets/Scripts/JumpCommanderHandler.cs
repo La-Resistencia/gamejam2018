0eae9ca984d9ba6ee2a5f4be25f0d7f073dd6f27
Luis Yucra <luis.yucra@gmail.com>
2018-01-28 12:58:03 -0500

    cat can move

---
+++ Unity/NekoTransmission/Assets/Scripts/JumpCommanderHandler.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+
+public class JumpCommanderHandler: MonoBehaviour
+{
+    public Rigidbody2D CatRigidbody2D;
+
+    public void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        CatRigidbody2D.AddForce(5f*Vector2.right, ForceMode2D.Force);
+    }
+
+    public void Jump()
+    {
+        CatRigidbody2D.AddForce(10*Vector2.up, ForceMode2D.Impulse);
+    }
+}
