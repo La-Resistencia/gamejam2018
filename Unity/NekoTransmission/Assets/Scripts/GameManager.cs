dc44a95e73d66a984665a7ba2c3703d2c64ff327
Jose Fuentes Casa <ssoj.josss@gmail.com>
2018-01-27 22:48:09 -0500

    meta upload

---
+++ Unity/NekoTransmission/Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject player;
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Hola juego");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
